ec5dc8210a6fe27f91106872f85bdf960e628995
Samer Mujibuddin <WorkStation@Samers-MBP.attlocal.net>
2016-11-15 22:45:46 -0600

    Added all files

--- Tapper-Rerun-App/ViewController.swift
+++ Tapper-Rerun-App/ViewController.swift
@@ -9,17 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    
+    
+    //Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsText: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var coinButton: UIButton!
+    @IBOutlet weak var tapsLabel: UILabel!
+    
+    //Actions
+    @IBAction func onCoinButtonPressed(_sender: UIButton!) {
+        currentTaps += 1
+        updateLabel()
+        
+        if isGameOver() {
+            restartGame()
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onPlayButtonPressed(_sender: UIButton!) {
+        if howManyTapsText.text != nil && howManyTapsText.text != "" {
+        startOutlets()
+        maxTaps = Int(howManyTapsText.text!)!
+        updateLabel()
+        
+        }
+        
     }
-
+    func endOutlets(){
+        logoImg.isHidden = false
+        howManyTapsText.isHidden = false
+        playButton.isHidden = false
+        coinButton.isHidden = true
+        tapsLabel.isHidden = true
+    }
+    func startOutlets(){
+        logoImg.isHidden = true
+        howManyTapsText.isHidden = true
+        playButton.isHidden = true
+        coinButton.isHidden = false
+        tapsLabel.isHidden = false
+    }
+    func updateLabel(){
+        tapsLabel.text = "\(currentTaps) Taps"
+    }
+    func restartGame(){
+        maxTaps = 0
+        howManyTapsText.text = ""
+        endOutlets()
+    }
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+           return true
+        } else {
+            return false
+        }
+    }
 
 }
 
